60909f176085d0a562bbd3a666d492b5b403a6be
leducmanh0838@gmail.com <leducmanh0838@gmail.com>
2024-12-01 01:39:00 +0700

    chức năng đăng ký làm xong nhưng chức năng đăng nhập chưa làm xong

--- app/admin.py
+++ app/admin.py
@@ -1,232 +1,232 @@
-import cloudinary.uploader
-import wtforms
-from flask_admin import Admin, BaseView, expose
-from sqlalchemy import false
-from sqlalchemy.testing import fails
-from wtforms.fields.datetime import DateField
-
-from app import admin, db
-from flask_admin.contrib.sqla import ModelView
-from app.dao import get_role_name_by_role_id
-from app.models import Sach, QuyDinh, SoLuongCuonConLai, TacGia, TheLoai
-from flask_login import current_user, logout_user
-from flask import redirect
-from app.models import VaiTro
-from wtforms import StringField, SelectField, FileField, Form
-from wtforms.validators import DataRequired
-from flask_wtf.file import FileAllowed
-from markupsafe import Markup
-
-
-class MyView(BaseView):
-    def is_accessible(self):
-        return current_user.is_authenticated
-
-
-class AuthenticatedView(ModelView):
-    def is_accessible(self):
-        quan_ly_id = VaiTro.query.filter(VaiTro.ten_vai_tro.__eq__('QUANLY')).first()
-        return current_user.is_authenticated and current_user.vai_tro_id == quan_ly_id.id
-
-
-class AuthenticatedQuanLyKhoView(ModelView):
-    def is_accessible(self):
-        qlk = VaiTro.query.filter(VaiTro.ten_vai_tro.__eq__('QUANLYKHO')).first()
-        return current_user.is_authenticated and current_user.vai_tro_id == qlk.id
-
-
-
-class QuyDinhView(AuthenticatedView):
-    can_create = True
-    can_edit = True
-
-
-class RevenueStatsView(MyView):
-    @expose("/")
-    def index(self):
-        return self.render("admin/revenue-stats.html")
-
-
-class FrequencyStatsView(MyView):
-    @expose("/")
-    def index(self):
-        return self.render("admin/frequency-stats.html")
-
-
-class Logout(MyView):
-    @expose("/")
-    def index(self):
-        logout_user()
-        return redirect("/admin")
-
-
-class SachForm(Form):
-    ten_sach = StringField('Tên sách', validators=[DataRequired()])
-    don_gia = StringField('Đơn giá', validators=[DataRequired()])
-    tac_gia_id = SelectField('Tác giả', coerce=int, validators=[DataRequired()])
-    the_loai_id = SelectField('Thể loại', coerce=int, validators=[DataRequired()])
-    bia_sach = FileField('Bìa sách', validators=[
-        DataRequired(), FileAllowed(['jpg', 'jpeg', 'png', 'gif'], "Chỉ được phép upload file hình ảnh!")])
-
-
-class SachView(AuthenticatedView):
-    can_edit = True
-    can_view_details = True
-    column_searchable_list = ['id', 'ten_sach']
-    form = SachForm
-
-    def create_form(self):
-        form = super().create_form()
-        form.tac_gia_id.choices = [(a.id, a.ten_tac_gia) for a in TacGia.query.all()]
-        form.the_loai_id.choices = [(a.id, a.ten_the_loai) for a in TheLoai.query.all()]
-
-        return form
-
-    def edit_form(self, obj):
-        form = super().edit_form(obj)  # Gọi phương thức gốc
-        form.tac_gia_id.choices = [(a.id, a.ten_tac_gia) for a in TacGia.query.all()]  # Thêm danh sách tác giả
-        form.the_loai_id.choices = [(a.id, a.ten_the_loai) for a in TheLoai.query.all()]
-
-        return form
-
-    # form_excluded_columns=['hoa_don_ban_sach','phieu_nhap_sach','don_hang','hoa_don_ban_sach','so_luong_cuon_con_lai']# fields bị loại bỏ trong form
-
-    column_list = ['id', 'ten_sach', 'don_gia', 'so_luong_cuon_con_lai']  # cot hiển thị
-
-    column_labels = {  # sua ten hien thi
-        'id': 'Mã SP',
-        'ten_sach': 'Tên sách',
-        'don_gia': 'Đơn giá',
-        'tac_gia_id': 'Tác giả',
-        'the_loai_id': 'Thể loại',
-        'so_luong_cuon_con_lai': 'Số lượng',
-        'bia_sach': 'Bìa sách'
-    }
-    #
-    # form_overrides = {
-    #     'bia_sach': ImageUploadField,  # cho upload file
-    #     'the_loai_id':QuerySelectField
-    # }
-    #
-    # form_args = {
-    #     'bia_sach':{
-    #             'label':'Upload image'
-    #     },
-    #     'the_loai_id': {
-    #         'label':'Thể loại',
-    #         'query_factory': lambda: TheLoai.query.all(),  # Truy vấn tất cả thể loại
-    #         'get_label': 'ten_the_loai',  # Hiển thị tên thể loại trong dropdown
-    #         'allow_blank': False  # Không cho phép bỏ trống trường này
-    #     },
-    #     'tac_gia_id': {
-    #         'label':'Tác giả',
-    #         'query_factory': lambda: TacGia.query.all(),
-    #         'get_label': 'ten_tac_gia',
-    #         'allow_blank': False
-    #     }
-    # }
-    #
-    column_formatters = {
-        'so_luong_cuon_con_lai': lambda v, c, m, p: (
-                db.session.query(SoLuongCuonConLai.so_luong).filter(SoLuongCuonConLai.sach_id == m.id).order_by(
-                    SoLuongCuonConLai.thoi_diem.desc()).first() or 'NaN'
-        )
-    }
-    column_formatters_detail = {
-        'bia_sach': lambda v, c, m, p: Markup(
-            f'<img src="{m.bia_sach}" style="max-width: 200px; max-height: 150px;" alt="Bìa sách">'
-        ) if m.bia_sach else Markup('<p>No Image</p>')
-    }
-
-    def on_model_change(self, form, model, is_created):
-        file_data = form.bia_sach.data
-        if file_data:
-
-            if not file_data.content_type.startswith("image/"):
-                raise ValueError("Chỉ được upload hình ảnh (JPEG, PNG, GIF, v.v.)")
-
-            upload_result = cloudinary.uploader.upload(file_data, folder="upload/bia_sach")
-            model.bia_sach = upload_result.get('secure_url')
-        model.tac_gia_id = form.tac_gia_id.data
-        return super().on_model_change(form, model, is_created)
-
-
-class TacGiaView(AuthenticatedView):
-    column_searchable_list = ['ten_tac_gia']
-    form_excluded_columns = ['sach']
-    column_list = ['id', 'ten_tac_gia']
-    column_labels = {
-        'id': 'Mã tác giả',
-        'ten_tac_gia': 'Tên tác giả'
-    }
-
-
-
-class TheLoaiView(AuthenticatedView):
-    column_searchable_list = ['ten_the_loai']
-    form_excluded_columns = ['sach']
-    column_list = ['id', 'ten_the_loai']
-    column_labels = {
-        'id': 'Mã thể loại',
-        'ten_the_loai': 'Tên thể loại'
-    }
-
-class UserView(AuthenticatedView):
-    column_searchable_list = ['id','ho','ten','username']
-    can_edit = True
-    can_create = True
-    form_widget_args = {
-        'ngay_tao': {
-            'disabled': True
-        }
-    }
-
-
-class PhieuNhapSachForm(Form):
-    ngay_nhap = DateField('Ngày nhập')
-    user = SelectField('Người quản lý kho',coerce=int)
-
-class PhieuNhapSachView(AuthenticatedQuanLyKhoView):
-    column_list = ['id','quan_ly_kho_id','ngay_nhap']
-    form_excluded_columns = ['sach']
-    can_view_details = True
-    column_searchable_list = ['id','quan_ly_kho_id']
-    can_edit = False
-    can_create = True
-
-
-    form_widget_args = {
-        'ngay_nhap':{
-            'disabled' : True
-        },
-        'user':{
-                 'disabled': True
-    }
-
-    }
-    column_labels = {
-        'id': 'Mã phiếu',
-        'quan_ly_kho_id': 'Mã người quản lý kho',
-        'ngay_nhap': 'Ngày nhập'
-    }
-
-
-class NhapSach(AuthenticatedQuanLyKhoView):
-    @expose("/")
-    def index(self):
-        return self.render("admin/bookimport.html")
-
-
-admin.add_view(UserView(User,db.session,name='Quản lý User'))
-admin.add_view(SachView(Sach, db.session, name='Sách', category='Quản lý sách'))
-admin.add_view(TheLoaiView(TheLoai, db.session, name='Thể loại', category='Quản lý sách'))
-admin.add_view(TacGiaView(TacGia, db.session, name='Tác giả', category='Quản lý sách'))
-admin.add_view(QuyDinhView(QuyDinh, db.session, name='Quy định'))
-
-admin.add_view(PhieuNhapSachView(PhieuNhapSach,db.session,name='Phiếu nhập sách',category='Nhập sách'))#
-
-admin.add_view(RevenueStatsView(name='Thống kê doanh thu', category='Thống kê báo cáo'))
-admin.add_view(FrequencyStatsView(name='Thống kê tần suất', category='Thống kê báo cáo'))
-admin.add_view(NhapSach(name='Nhập sách'))
-
-admin.add_view(Logout(name="Đăng xuất"))
+# import cloudinary.uploader
+# import wtforms
+# from flask_admin import Admin, BaseView, expose
+# from sqlalchemy import false
+# from sqlalchemy.testing import fails
+# from wtforms.fields.datetime import DateField
+#
+# from app import admin, db
+# from flask_admin.contrib.sqla import ModelView
+# from app.dao import get_role_name_by_role_id
+# from app.models import Sach, QuyDinh, SoLuongCuonConLai, TacGia, TheLoai
+# from flask_login import current_user, logout_user
+# from flask import redirect
+# from app.models import VaiTro
+# from wtforms import StringField, SelectField, FileField, Form
+# from wtforms.validators import DataRequired
+# from flask_wtf.file import FileAllowed
+# from markupsafe import Markup
+#
+#
+# class MyView(BaseView):
+#     def is_accessible(self):
+#         return current_user.is_authenticated
+#
+#
+# class AuthenticatedView(ModelView):
+#     def is_accessible(self):
+#         quan_ly_id = VaiTro.query.filter(VaiTro.ten_vai_tro.__eq__('QUANLY')).first()
+#         return current_user.is_authenticated and current_user.vai_tro_id == quan_ly_id.id
+#
+#
+# class AuthenticatedQuanLyKhoView(ModelView):
+#     def is_accessible(self):
+#         qlk = VaiTro.query.filter(VaiTro.ten_vai_tro.__eq__('QUANLYKHO')).first()
+#         return current_user.is_authenticated and current_user.vai_tro_id == qlk.id
+#
+#
+#
+# class QuyDinhView(AuthenticatedView):
+#     can_create = True
+#     can_edit = True
+#
+#
+# class RevenueStatsView(MyView):
+#     @expose("/")
+#     def index(self):
+#         return self.render("admin/revenue-stats.html")
+#
+#
+# class FrequencyStatsView(MyView):
+#     @expose("/")
+#     def index(self):
+#         return self.render("admin/frequency-stats.html")
+#
+#
+# class Logout(MyView):
+#     @expose("/")
+#     def index(self):
+#         logout_user()
+#         return redirect("/admin")
+#
+#
+# class SachForm(Form):
+#     ten_sach = StringField('Tên sách', validators=[DataRequired()])
+#     don_gia = StringField('Đơn giá', validators=[DataRequired()])
+#     tac_gia_id = SelectField('Tác giả', coerce=int, validators=[DataRequired()])
+#     the_loai_id = SelectField('Thể loại', coerce=int, validators=[DataRequired()])
+#     bia_sach = FileField('Bìa sách', validators=[
+#         DataRequired(), FileAllowed(['jpg', 'jpeg', 'png', 'gif'], "Chỉ được phép upload file hình ảnh!")])
+#
+#
+# class SachView(AuthenticatedView):
+#     can_edit = True
+#     can_view_details = True
+#     column_searchable_list = ['id', 'ten_sach']
+#     form = SachForm
+#
+#     def create_form(self):
+#         form = super().create_form()
+#         form.tac_gia_id.choices = [(a.id, a.ten_tac_gia) for a in TacGia.query.all()]
+#         form.the_loai_id.choices = [(a.id, a.ten_the_loai) for a in TheLoai.query.all()]
+#
+#         return form
+#
+#     def edit_form(self, obj):
+#         form = super().edit_form(obj)  # Gọi phương thức gốc
+#         form.tac_gia_id.choices = [(a.id, a.ten_tac_gia) for a in TacGia.query.all()]  # Thêm danh sách tác giả
+#         form.the_loai_id.choices = [(a.id, a.ten_the_loai) for a in TheLoai.query.all()]
+#
+#         return form
+#
+#     # form_excluded_columns=['hoa_don_ban_sach','phieu_nhap_sach','don_hang','hoa_don_ban_sach','so_luong_cuon_con_lai']# fields bị loại bỏ trong form
+#
+#     column_list = ['id', 'ten_sach', 'don_gia', 'so_luong_cuon_con_lai']  # cot hiển thị
+#
+#     column_labels = {  # sua ten hien thi
+#         'id': 'Mã SP',
+#         'ten_sach': 'Tên sách',
+#         'don_gia': 'Đơn giá',
+#         'tac_gia_id': 'Tác giả',
+#         'the_loai_id': 'Thể loại',
+#         'so_luong_cuon_con_lai': 'Số lượng',
+#         'bia_sach': 'Bìa sách'
+#     }
+#     #
+#     # form_overrides = {
+#     #     'bia_sach': ImageUploadField,  # cho upload file
+#     #     'the_loai_id':QuerySelectField
+#     # }
+#     #
+#     # form_args = {
+#     #     'bia_sach':{
+#     #             'label':'Upload image'
+#     #     },
+#     #     'the_loai_id': {
+#     #         'label':'Thể loại',
+#     #         'query_factory': lambda: TheLoai.query.all(),  # Truy vấn tất cả thể loại
+#     #         'get_label': 'ten_the_loai',  # Hiển thị tên thể loại trong dropdown
+#     #         'allow_blank': False  # Không cho phép bỏ trống trường này
+#     #     },
+#     #     'tac_gia_id': {
+#     #         'label':'Tác giả',
+#     #         'query_factory': lambda: TacGia.query.all(),
+#     #         'get_label': 'ten_tac_gia',
+#     #         'allow_blank': False
+#     #     }
+#     # }
+#     #
+#     column_formatters = {
+#         'so_luong_cuon_con_lai': lambda v, c, m, p: (
+#                 db.session.query(SoLuongCuonConLai.so_luong).filter(SoLuongCuonConLai.sach_id == m.id).order_by(
+#                     SoLuongCuonConLai.thoi_diem.desc()).first() or 'NaN'
+#         )
+#     }
+#     column_formatters_detail = {
+#         'bia_sach': lambda v, c, m, p: Markup(
+#             f'<img src="{m.bia_sach}" style="max-width: 200px; max-height: 150px;" alt="Bìa sách">'
+#         ) if m.bia_sach else Markup('<p>No Image</p>')
+#     }
+#
+#     def on_model_change(self, form, model, is_created):
+#         file_data = form.bia_sach.data
+#         if file_data:
+#
+#             if not file_data.content_type.startswith("image/"):
+#                 raise ValueError("Chỉ được upload hình ảnh (JPEG, PNG, GIF, v.v.)")
+#
+#             upload_result = cloudinary.uploader.upload(file_data, folder="upload/bia_sach")
+#             model.bia_sach = upload_result.get('secure_url')
+#         model.tac_gia_id = form.tac_gia_id.data
+#         return super().on_model_change(form, model, is_created)
+#
+#
+# class TacGiaView(AuthenticatedView):
+#     column_searchable_list = ['ten_tac_gia']
+#     form_excluded_columns = ['sach']
+#     column_list = ['id', 'ten_tac_gia']
+#     column_labels = {
+#         'id': 'Mã tác giả',
+#         'ten_tac_gia': 'Tên tác giả'
+#     }
+#
+#
+#
+# class TheLoaiView(AuthenticatedView):
+#     column_searchable_list = ['ten_the_loai']
+#     form_excluded_columns = ['sach']
+#     column_list = ['id', 'ten_the_loai']
+#     column_labels = {
+#         'id': 'Mã thể loại',
+#         'ten_the_loai': 'Tên thể loại'
+#     }
+#
+# class UserView(AuthenticatedView):
+#     column_searchable_list = ['id','ho','ten','username']
+#     can_edit = True
+#     can_create = True
+#     form_widget_args = {
+#         'ngay_tao': {
+#             'disabled': True
+#         }
+#     }
+#
+#
+# class PhieuNhapSachForm(Form):
+#     ngay_nhap = DateField('Ngày nhập')
+#     user = SelectField('Người quản lý kho',coerce=int)
+#
+# class PhieuNhapSachView(AuthenticatedQuanLyKhoView):
+#     column_list = ['id','quan_ly_kho_id','ngay_nhap']
+#     form_excluded_columns = ['sach']
+#     can_view_details = True
+#     column_searchable_list = ['id','quan_ly_kho_id']
+#     can_edit = False
+#     can_create = True
+#
+#
+#     form_widget_args = {
+#         'ngay_nhap':{
+#             'disabled' : True
+#         },
+#         'user':{
+#                  'disabled': True
+#     }
+#
+#     }
+#     column_labels = {
+#         'id': 'Mã phiếu',
+#         'quan_ly_kho_id': 'Mã người quản lý kho',
+#         'ngay_nhap': 'Ngày nhập'
+#     }
+#
+#
+# class NhapSach(AuthenticatedQuanLyKhoView):
+#     @expose("/")
+#     def index(self):
+#         return self.render("admin/bookimport.html")
+#
+#
+# admin.add_view(UserView(User,db.session,name='Quản lý User'))
+# admin.add_view(SachView(Sach, db.session, name='Sách', category='Quản lý sách'))
+# admin.add_view(TheLoaiView(TheLoai, db.session, name='Thể loại', category='Quản lý sách'))
+# admin.add_view(TacGiaView(TacGia, db.session, name='Tác giả', category='Quản lý sách'))
+# admin.add_view(QuyDinhView(QuyDinh, db.session, name='Quy định'))
+#
+# admin.add_view(PhieuNhapSachView(PhieuNhapSach,db.session,name='Phiếu nhập sách',category='Nhập sách'))#
+#
+# admin.add_view(RevenueStatsView(name='Thống kê doanh thu', category='Thống kê báo cáo'))
+# admin.add_view(FrequencyStatsView(name='Thống kê tần suất', category='Thống kê báo cáo'))
+# admin.add_view(NhapSach(name='Nhập sách'))
+#
+# admin.add_view(Logout(name="Đăng xuất"))
--- app/index.py
+++ app/index.py
@@ -30,6 +30,54 @@ def login_admin_process():
     return redirect('/admin')
 
 
+@app.route('/register/', methods=['get','post'])
+def register():
+    err_msg=''
+    if request.method.__eq__('POST'):
+        password = request.form['password']
+        confirm = request.form['confirm']
+        if password.__eq__(confirm):
+            #avatar
+            avatar=''
+            if request.files['avatar']:
+                res=cloudinary.uploader.upload(request.files['avatar'])
+                avatar = res['secure_url']
+
+            #save user
+            try:
+                dao.create_user(ten=request.form['firstname'],
+                                ho=request.form['lastname'],
+                             username=request.form['username'],
+                             password=request.form['password'],
+                             avatar=avatar,
+                                vaitro_id=4)
+                return redirect('/login')
+            except:
+                err_msg='Hệ thống có lỗi'
+        else:
+            err_msg='mật khẩu KHÔNG khớp'
+    return render_template("register.html", err_msg=err_msg)
+
+
+@app.route('/login/', methods=['get','post'])
+# @annonymous_user
+def login_my_user():
+    if request.method.__eq__('POST'):
+        username = request.form['username']
+        password = request.form['password']
+        user = dao.auth_user(username, password)
+        if user:
+            login_user(user=user)
+            return redirect('/register')
+
+    return render_template("login.html")
+
+
+@app.route('/logout/')
+def logout_my_user():
+    logout_user()
+    return redirect('/login')
+
 
 if __name__ == "__main__":
     with app.app_context():
