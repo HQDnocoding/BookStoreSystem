3dfb3860e8fb846057f3d1a27552296ba6a3f596
HQDNocoding <hqd11lv@gmail.com>
2024-12-24 14:14:55 +0700

    .

--- app/models.py
+++ app/models.py
@@ -5,14 +5,14 @@ from datetime import date
 from sqlalchemy import Column, Integer, Text, String, DateTime, Float, Boolean, ForeignKey, UniqueConstraint, Date
 from sqlalchemy.orm import relationship, backref
 from datetime import datetime
-from app import app as my_app, db, Role, Status, PayingMethod
+from app import app as my_app, db, Role, Status, PayingMethod, Rule 
 from flask_login import UserMixin
 
 
 class VaiTro(db.Model):
     __tablename__ = 'vai_tro'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    ten_vai_tro = Column(String(50), nullable=True, unique=True)
+    ten_vai_tro = Column(String(50), nullable=True)
     user = relationship('User', backref='vai_tro', lazy=True)
 
     # def __int__(self):
@@ -70,16 +70,17 @@ class User(db.Model, UserMixin):
     vai_tro_id = Column(Integer, ForeignKey(VaiTro.id), nullable=False)
     phieu_nhap_sach = relationship('PhieuNhapSach', backref='user', lazy=True)
     don_hang = relationship('DonHang', backref='user', lazy=True)
-    hoa_don_ban_sach=relationship('HoaDonBanSach',backref='user',lazy=True)
+    # hoa_don_ban_sach=relationship('HoaDonBanSach',backref='user',lazy=True)
+    nhan_vien=relationship('NhanVien_DonHang',backref='user',lazy=True)
     def __str__(self):
         return self.ten
 
-class HoaDonBanSach(db.Model):
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    ngay_tao_hoa_don = Column(DateTime, nullable=False, default=datetime.now())
-
-    nhan_vien = Column(Integer, ForeignKey(User.id), nullable=True)
-    sach = relationship('ChiTietHoaDon', backref='hoa_don_ban_sach')
+# class HoaDonBanSach(db.Model):
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     ngay_tao_hoa_don = Column(DateTime, nullable=False, default=datetime.now())
+#
+#     nhan_vien = Column(Integer, ForeignKey(User.id), nullable=True)
+#     sach = relationship('ChiTietHoaDon', backref='hoa_don_ban_sach')
 
 
 class Sach(db.Model):
@@ -94,30 +95,29 @@ class Sach(db.Model):
     the_loai_id = Column(Integer, ForeignKey(TheLoai.id), nullable=False)
     tac_gia_id = Column(Integer, ForeignKey(TacGia.id), nullable=False)
 
-    hoa_don_ban_sach = relationship('ChiTietHoaDon', backref='sach')
-    so_luong_cuon_con_lai = relationship('SoLuongCuonConLai', backref='sach', lazy=True)
+    # hoa_don_ban_sach = relationship('ChiTietHoaDon', backref='sach')
     phieu_nhap_sach = relationship('ChiTietPhieuNhapSach', backref='sach')
     don_hang = relationship('ChiTietDonHang', backref='sach')
 
     def __str__(self):
         return self.ten_sach
 
-class ChiTietHoaDon(db.Model):
-    sach_id = Column(ForeignKey(Sach.id), primary_key=True)
-    hoa_don_id = Column(ForeignKey(HoaDonBanSach.id), primary_key=True)
-    so_luong = Column(Integer, nullable=False)
-    tong_tien = Column(Float, nullable=False)
+# class ChiTietHoaDon(db.Model):
+#     sach_id = Column(ForeignKey(Sach.id), primary_key=True)
+#     hoa_don_id = Column(ForeignKey(HoaDonBanSach.id), primary_key=True)
+#     so_luong = Column(Integer, nullable=False)
+#     tong_tien = Column(Float, nullable=False)
 
 
-class SoLuongCuonConLai(db.Model):
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    so_luong = Column(Integer, nullable=False)
-    thoi_diem = Column(DateTime, nullable=False, default=datetime.now())
-
-    sach_id = Column(Integer, ForeignKey(Sach.id), nullable=False)
-
-    def __int__(self):
-        return self.so_luong
+# class SoLuongCuonConLai(db.Model):
+#     id = Column(Integer, primary_key=True, autoincrement=True)
+#     so_luong = Column(Integer, nullable=False)
+#     thoi_diem = Column(DateTime, nullable=False, default=datetime.now())
+#
+#     sach_id = Column(Integer, ForeignKey(Sach.id), nullable=False)
+#
+#     def __int__(self):
+#         return self.so_luong
 
 
 # class ChiTietChinhSuaQuyDinh(db.Model):
@@ -138,7 +138,7 @@ class PhieuNhapSach(db.Model):
          return self.id
 
 class ChiTietPhieuNhapSach(db.Model):
-    phieu_nhap_sach_id = Column(Integer, ForeignKey(PhieuNhapSach.id),
+    phieu_nhap_sach_id = Column(Integer, ForeignKey(PhieuNhapSach.id, ondelete='CASCADE'),
                                 primary_key=True)
     sach_id = Column(Integer, ForeignKey(Sach.id), primary_key=True)
 
@@ -153,16 +153,25 @@ class DonHang(db.Model):
 
     phuong_thuc_id = Column(Integer, ForeignKey(PhuongThucThanhToan.id), nullable=False)
     trang_thai_id = Column(Integer, ForeignKey(TrangThaiDonHang.id), nullable=False)
-    khach_hang_id = Column(Integer, ForeignKey(User.id), nullable=False)
+    khach_hang_id = Column(Integer, ForeignKey(User.id), nullable=True)
+    # nhan_vien_id=Column(Integer,ForeignKey(User.id),nullable=False,default=1)
     thong_tin_nhan_hang = relationship('ThongTinNhanHang', uselist=False, backref='don_hang')
+    don_hang = relationship('NhanVien_DonHang', uselist=False, backref='don_hang')
     sach = relationship('ChiTietDonHang', backref='don_hang')
 
     # def __class__(self):
     #      return self
 
 
+class NhanVien_DonHang(db.Model):
+    donhang_id=Column(Integer,ForeignKey(DonHang.id, ondelete='CASCADE'),primary_key=True,nullable=False)
+    nhanvien_id=Column(Integer,ForeignKey(User.id),primary_key=False,nullable=False)
+
+    ngay_than_toan=Column(DateTime, nullable=False, default=datetime.now())
+
+
 class ThongTinNhanHang(db.Model):
-    id = Column(Integer, ForeignKey(DonHang.id), primary_key=True)
+    id = Column(Integer, ForeignKey(DonHang.id,ondelete='CASCADE'), primary_key=True)
     dien_thoai_nhan_hang = Column(String(50), nullable=False)
     dia_chi_nhan_hang = Column(String(225), nullable=False)
 
@@ -215,11 +224,20 @@ if __name__ == "__main__":
         # db.session.add_all([r1,r2,r3,r4])
 
         admin=User(ho='Hứa',ten="Hứa",username='admin1',password=pw,vai_tro_id=1)
-        # nhan_vien=User(ho='Trump',ten='Donald',username='nhanvien',password=pw,vai_tro_id=2)
-        # qlk=User(ho='Trump',ten='Donald',username='qlk',password=pw,vai_tro_id=3)
-        # u=User(ho='Trump',ten='Donald',username='client',password=pw,vai_tro_id=4)
-        # #
+        nhan_vien=User(ho='Trump',ten='Donald',username='nhanvien',password=pw,vai_tro_id=2)
+        qlk=User(ho='Trump',ten='Donald',username='qlk',password=pw,vai_tro_id=3)
+        u=User(ho='Trump',ten='Donald',username='client',password=pw,vai_tro_id=4)
+        #
         db.session.add_all([admin])
 
-
+        #
+        # ten_qd1=Rule.SL_NHAP_MIN.value
+        # ten_qd2=Rule.SL_MIN_TO_NHAP.value
+        # ten_qd3=Rule.OUT_OF_TIME_TO_PAY.value
+        # qd1=QuyDinh(ten_quy_dinh=ten_qd1, noi_dung='Số lượng tối thiểu khi nhập sách', gia_tri=150)
+        # qd2=QuyDinh(ten_quy_dinh=ten_qd2,noi_dung='Số lượng tối thiểu của đầu sách để được nhập',gia_tri=300)
+        # qd3=QuyDinh(ten_quy_dinh=ten_qd3,noi_dung='Số giờ tối đa kể từ khi đặt hàng đến lúc thanh toán',gia_tri=48)
+        #
+        # db.session.add_all([qd1,qd2,qd3])
+        #
         db.session.commit()
